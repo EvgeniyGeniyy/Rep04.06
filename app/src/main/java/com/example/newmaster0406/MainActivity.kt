da3c133675b6158d6af796261c0f8117bdb64b5a
EvgeniyGeniy <polyakovskiy1996@mail.ru>
2024-06-05 20:58:06 +0700

    Урок 22 Создание Класса и функций

--- app/src/main/java/com/example/newmaster0406/MainActivity.kt
+++ app/src/main/java/com/example/newmaster0406/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.newmaster0406
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        var user = User("Sergey", "000000", 25)
+        var user2 = User("Andrey", "11111", 32)
+        user.addAge(10)
+        user2.addAge(2)
+
+        user.printUserInfo()
+        user2.printUserInfo()
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/newmaster0406/User.kt
@@ -0,0 +1,16 @@
+package com.example.newmaster0406
+
+import android.util.Log
+
+class User(var name: String, var id: String, var age: Int) {
+
+    fun addAge(years: Int){
+        age = age.plus(years)
+
+    }
+    fun printUserInfo(){
+        Log.d("MyLog", "User name: $name User id: $id User age: $age")
+    }
+
+
+}
